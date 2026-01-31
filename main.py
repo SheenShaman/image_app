43ef61d1f930224669d355ea95f2cc795baae40b
Danila Koryakin <91043795+SheenShaman@users.noreply.github.com>
2026-01-31 19:21:27 +0300

    Danila/feature/main window (#2)
    * add_main_window
    
    * fix_main
    
    * add_ruff

--- main.py
+++ main.py
@@ -1,38 +1,10 @@
 import sys
-from PyQt5.QtWidgets import (
-    QApplication,
-    QWidget,
-    QLabel,
-    QVBoxLayout,
-    QPushButton,
-)
-from PyQt5.QtCore import Qt
 
+from PyQt5.QtWidgets import QApplication
 
-def main():
-    """
-    Проверка работоспособности
-    """
-    app = QApplication(sys.argv)
-
-    window = QWidget()
-    window.setWindowTitle("PyQt5 тест")
-    window.resize(400, 200)
-
-    layout = QVBoxLayout(window)
-
-    label = QLabel("✅ PyQt5 работает")
-    label.setAlignment(Qt.AlignCenter)
-
-    button = QPushButton("Закрыть")
-    button.clicked.connect(app.quit)
-
-    layout.addWidget(label)
-    layout.addWidget(button)
-
-    window.show()
-    sys.exit(app.exec_())
-
+from main_window import MainWindow
 
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    sys.exit(app.exec_())
